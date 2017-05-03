d6c3bc5246cea87381c2179058fddcc8c8b0a268
mykle1 <themykle@imykle.com>
2017-05-02 20:37:00 -0400

    Add files via upload

--- MMM-Fortune.js
+++ MMM-Fortune.js
@@ -1,126 +1,120 @@
-/* Magic Mirror
- * Module: MMM-Fortune
- *
- * By Mykle1
- * 
- */
-Module.register("MMM-Fortune", {
+   /* Magic Mirror
+    * Module: MMM-Fortune
+    *
+    * By Mykle1
+    * 
+    */
+   Module.register("MMM-Fortune", {
 
-    // Module config defaults.
-    defaults: {
-        mode: "Natural", // Natural, Enhanced, Lunar, naturalThumb, enhancedThumb
-        updateInterval: 30 * 60 * 1000, // 30 minutes - Don't change!
-        animationSpeed: 3000,
-        initialLoadDelay: 1250,
-        retryDelay: 2500,
-        useHeader: false,
-        header: "********Please set header txt in config.js***** see README",
-        maxWidth: "50%", // Should be the same as MaxHeight
-        maxHeight: "50%", // Should be the same as MaxWidth
-        rotateInterval: 10 * 1000,
+       // Module config defaults.
+       defaults: {
+           updateInterval: 60 * 60 * 1000, // every hour
+           fadeSpeed: 3000,
+           initialLoadDelay: 1250, // ms seconds delay
+           retryDelay: 2500,
+           header: "Good luck!",
+           maxWidth: "400px",
+       },
 
-    },
+       // Define required scripts.
+       getScripts: function() {
+           return ["moment.js"];
+       },
 
-    // Define required scripts.
-    getScripts: function() {
-        return ["moment.js"];
-    },
+       getStyles: function() {
+           return ["MMM-Fortune.css", "font-awesome.css"];
+       },
 
-    start: function() {
-        Log.info("Starting module: " + this.name);
+       // Define start sequence.
+       start: function() {
+           Log.info("Starting module: " + this.name);
 
-        // Set locale.
-        this.today = "";
-        this.fortune = [];
-		this.url = "http://fortunecookieapi.herokuapp.com/v1/cookie?fortuneId=&lottoId=&lessonId=&limit=";
-        this.activeItem = 0;
-        this.rotateInterval = null;
-        this.scheduleUpdate();
-    },
+           // Set locale.
+           moment.locale(config.language);
 
-    getDom: function() {
-		
-		var fortune = this.fortune;
+           this.today = "";
+           this.fortune = [];
+           this.url = "http://fortunecookieapi.herokuapp.com/v1/cookie?fortuneId=&lottoId=&lessonId=&limit=";
+           this.scheduleUpdate();
+       },
 
-        var wrapper = document.createElement("div");
-        wrapper.className = "wrapper";
-		wrapper.style.maxWidth = this.config.maxWidth;
+       getDom: function() {
 
-        if (!this.loaded) {
-            wrapper.innerHTML = "Reading your fortune...";
-            wrapper.className = "bright light small";
-            return wrapper;
-        }
-		if (this.config.header != "") {
+           var fortune = this.fortune;
+
+           var wrapper = document.createElement("div");
+           wrapper.className = "wrapper";
+           wrapper.style.maxWidth = this.config.maxWidth;
+
+
+           if (!this.loaded) {
+               wrapper.innerHTML = "Telling your fortune...";
+               wrapper.className = "bright light small";
+               return wrapper;
+           }
+           if (this.config.header != "") {
                var header = document.createElement("header");
                header.className = "header";
                header.innerHTML = this.config.header;
                wrapper.appendChild(header);
-        }
+           }
 
-		var top = document.createElement("div");
+           var top = document.createElement("div");
            top.classList.add("content");
-		   
-		var fortuneLogo = document.createElement("div");
+
+        /* var fortuneLogo = document.createElement("div");
            var fortuneIcon = document.createElement("img");
            fortuneIcon.src = fortune.strDrinkThumb;
            fortuneIcon.classList.add("imgDes");
            fortuneLogo.appendChild(fortuneIcon);
            top.appendChild(fortuneLogo);
-
-		   var title = document.createElement("h3");
+		*/
+           var title = document.createElement("h3");
            title.classList.add("small");
-           if (fortune.message === " ") {
-               title.innerHTML = fortune.message + " YES ";
+           if (this.fortune === " ") {
+               title.innerHTML = fortune.message + " Marker 1 ";
            } else {
-               title.innerHTML = fortune.id + "  YEAH  "; // fortune.id undefined
+               title.innerHTML = this.fortune + "  Marker 2  "; // this.fortune NULL?
            }
            top.appendChild(title);
-		
-        console.log("error6");
-		   
+
 
            var des = document.createElement("p");
            des.classList.add("xsmall", "bright");
-           des.innerHTML = fortune.message + " " + fortune.english + " - " + fortune.chinese + " " + fortune.pronunciation;
-           top.appendChild(des); // message, english, chinese, pronunciation undefined
-console.log("error7");
+           des.innerHTML = this.fortune + " Marker 3 "; // this.fortune NULL?
+           top.appendChild(des);
+
            wrapper.appendChild(top);
+           return wrapper;
 
-console.log("error8");
-		  
-		  return wrapper;
+       },
 
-        },
-        
-    processFortune: function(data) {
-        this.today = data.Today;
-        this.fortune = data;
-        this.loaded = true;
-    },
+       processFortune: function(data) {
+           //	console.log(data);
+           //this.today = data.Today;
+           this.fortune = data;
+           this.loaded = true;
+       },
 
-	
-    scheduleUpdate: function() {
-        setInterval(() => {
-            this.getFortune();
-        }, this.config.updateInterval);
-        this.getFortune(this.config.initialLoadDelay);
-     //   var self = this;
-    },
+       scheduleUpdate: function() {
+           setInterval(() => {
+               this.getFortune();
+           }, this.config.updateInterval);
+
+           this.getFortune(this.config.initialLoadDelay);
+       },
 
 
-    getFortune: function() {
-        this.sendSocketNotification('GET_FORTUNE', this.url);
-    },
+       getFortune: function() {
+           this.sendSocketNotification('GET_FORTUNE', this.url);
+       },
 
+       socketNotificationReceived: function(notification, payload) {
+           if (notification === "FORTUNE_RESULT") {
+               this.processFortune(payload);
+               this.updateDom(this.config.fadeSpeed);
+           }
+           this.updateDom(this.config.initialLoadDelay);
+       },
 
-    socketNotificationReceived: function(notification, payload) {
-        if (notification === "FORTUNE_RESULTS") {
-            this.processFortune(payload);
-			this.updateDom(this.config.fadeSpeed);
-        }
-
-        this.updateDom(this.config.initialLoadDelay);
-    },
-
-});
+   });
--- node_helper.js
+++ node_helper.js
@@ -12,18 +12,9 @@ module.exports = NodeHelper.create({
 
     start: function() {
         this.fortune = {
-            timestamp: null,
             data: null
+			
         };
-        this.path = "modules/MMM-Fortune/fortune.json";
-        if (fs.existsSync(this.path)) {
-            var temp = JSON.parse(fs.readFileSync(this.path, 'utf8'));
-            if (temp.timestamp === this.getDate()) {
-                this.fortune = temp;
-            }
-            //console.log(temp);
-        }
-
     },
 
     getFortune: function(url) {
@@ -32,33 +23,20 @@ module.exports = NodeHelper.create({
             method: 'GET'
         }, (error, response, body) => {
             if (!error && response.statusCode == 200) {
-                var result = JSON.parse(body).fortune[0];
+                var result = JSON.parse(body).fortune;
 				console.log(result);
-                this.sendSocketNotification('FORTUNE_RESULT', result);
-                this.fortune.timestamp = this.getDate();
+                this.sendSocketNotification('FORTUNE_RESULT', result);                
                 this.fortune.data = result;
-                this.fileWrite();
+                
             }
         });
     },
 
-    fileWrite: function() {
-        fs.writeFile(this.path, JSON.stringify(this.fortune), function(err) {
-            if (err) {
-                return console.log(err);
-            }
-            console.log("The Fortune file was saved!");
-        });
-    },
-
-    getDate: function() {
-        return (new Date()).toLocaleDateString();
-    },
-
+    
     //Subclass socketNotificationReceived received.
     socketNotificationReceived: function(notification, payload) {
         if (notification === 'GET_FORTUNE') {
-            if (this.fortune.timestamp === this.getDate() && this.fortune.data !== null) {
+            if (this.fortune.data !== null) {
                 this.sendSocketNotification('FORTUNE_RESULT', this.fortune.data);
             } else {
                 this.getFortune(payload);
